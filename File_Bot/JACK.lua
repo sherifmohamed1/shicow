13a104fc90eb016f699d60d84d150d25c7eecba1
sherifmohamed1 <89645416+sherifmohamed1@users.noreply.github.com>
2021-11-24 05:02:24 +0200

    Add files via upload

--- File_Bot/JACK.lua
+++ File_Bot/JACK.lua
@@ -192,6 +192,18 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendanimation?chat_id=' .. msg.chat_id_ .. '&animation=https://t.me/SH_TI_N/78&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
+if text == 'بالي' then
+local Text = [[
+[قمر البار.. 🌚❤️](t.me/bali610)
+]]
+keyboard = {} 
+keyboard.inline_keyboard = {
+{{text = '⇣❤️🌚بالي القمر⇣', url="t.me/bali610"}},
+}
+local msg_id = msg.id_/2097152/0.5
+https.request("https://api.telegram.org/bot"..token..'/sendanimation?chat_id=' .. msg.chat_id_ .. '&animation=https://t.me/SH_TI_N/91&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+end
+
 if text == 'عامل اي' then 
 local my_ph = bot_data:get(ban_id.."my_GHoeq2:status"..msg.chat_id_)
 if not my_ph then
